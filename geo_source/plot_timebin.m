e4e2eeb50df63f176dc4f12363c709c6de1688ef
Howard E. Motteler <motteler@gmail.com>
2018-05-24 14:02:32 -0400

    moved old obs list functions to obs_list_source, updated the README

--- geo_source/plot_timebin.m
+++ geo_source/plot_timebin.m
@@ -2,10 +2,22 @@
 % plot_timebin - equal area time stats
 %
 
-addpath ./time
+addpath source
+addpath geo_data
+addpath /asl/packages/ccast/motmsc/time
 
-d1 = load('airs_obs_d1s2w1');
-d2 = load('cris_obs_d1s2w1');
+d1 = load('airs_geo_d1s2');
+d2 = load('cris_geo_d1s2');
+
+% % subset by secant of satellite zenith angle
+% ix1 = find(sec(deg2rad(d1.szen)) <= 1.8);
+% ix2 = find(sec(deg2rad(d2.szen)) <= 1.8);
+% d1.slat = d1.slat(ix1);
+% d1.slon = d1.slon(ix1);
+% d1.szen = d1.szen(ix1);
+% d2.slat = d2.slat(ix2);
+% d2.slon = d2.slon(ix2);
+% d2.szen = d2.szen(ix2);
 
 % nLat = 20;  dLon = 6;
   nLat = 24;  dLon = 4;
